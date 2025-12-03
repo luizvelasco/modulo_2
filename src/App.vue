a1154f486552cf1778732919124fb3d0112d581c
Luiz Velasco <luizvelasco@gmail.com>
2025-12-03 18:10:42 -0300

    v-if

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
+import { ref } from 'vue';
+const title = ref('Hello World 3');
+const showTitle = ref(true);
 </script>
 
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1 v-if="showTitle">{{ title }}</h1>
   </div>
 </template>
