216f35a94e16cc146ff781effa2aef05e3ca22e7
luizvelasco <luizvelasco@gmail.com>
2025-12-07 18:54:51 -0300

    eventos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
-  import { watch, ref } from 'vue';
-  const name = ref('');
-  
-  watch(name, (newValue, oldValue) => {
-    console.log(newValue)
+  import { ref } from 'vue';
+  const counter = ref(0);
+  const onClick = () => {
+    counter.value ++;
+  }
+
+  const onFocus = () => {
+    console.log('onFocus');
+  }
+
+  const onMouseover = () => {
+    console.log('onMouseover');
     
-  });
+  }
 
 </script>
 
 <template>
   <div>
-   <input type="text" v-model="name">
+    Contador: {{ counter }}
+    <button @click="onClick">
+      Click
+    </button>
+
+    <br>
+
+    <input type="text" @focus="onFocus" @mouseover="onMouseover">
   </div>
 </template>
